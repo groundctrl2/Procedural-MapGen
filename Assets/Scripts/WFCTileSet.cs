a8244ca43f7fbe6389586c9be49ae3528f710206
Tommy Collier <98130531+groundctrl2@users.noreply.github.com>
2025-07-29 14:57:26 -0600

    Simplified WFCTileSet test rules
    Simplified the ruleset to generate a striped pattern. This change is temporary.

--- Assets/Scripts/WFCTileSet.cs
+++ Assets/Scripts/WFCTileSet.cs
@@ -1,6 +1,4 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 /// <summary>
 /// Provides a predefined set of WFCTile configurations representing valid tile types and edge compatibility
@@ -18,74 +16,13 @@ public class WFCTileSet
     {
         return new List<WFCTile> {
             // Grass:
-            new WFCTile { type = WFCType.grass, up = WFCType.grass, right = WFCType.grass, down = WFCType.grass, left = WFCType.grass},
-            // Grass: 1 Sand
-            new WFCTile { type = WFCType.grass, up = WFCType.sand, right = WFCType.grass, down = WFCType.grass, left = WFCType.grass },
-            new WFCTile { type = WFCType.grass, up = WFCType.grass, right = WFCType.sand, down = WFCType.grass, left = WFCType.grass },
-            new WFCTile { type = WFCType.grass, up = WFCType.grass, right = WFCType.grass, down = WFCType.sand, left = WFCType.grass },
-            new WFCTile { type = WFCType.grass, up = WFCType.grass, right = WFCType.grass, down = WFCType.grass, left = WFCType.sand },
-            // Grass: 2 Sand (must be touching)
-            new WFCTile { type = WFCType.grass, up = WFCType.sand, right = WFCType.sand, down = WFCType.grass, left = WFCType.grass },
-            new WFCTile { type = WFCType.grass, up = WFCType.sand, right = WFCType.grass, down = WFCType.grass, left = WFCType.sand },
-            new WFCTile { type = WFCType.grass, up = WFCType.grass, right = WFCType.sand, down = WFCType.sand, left = WFCType.grass },
-            new WFCTile { type = WFCType.grass, up = WFCType.grass, right = WFCType.grass, down = WFCType.sand, left = WFCType.sand },
-            // Grass: 3 Sand
-            new WFCTile { type = WFCType.grass, up = WFCType.grass, right = WFCType.sand, down = WFCType.sand, left = WFCType.sand },
-            new WFCTile { type = WFCType.grass, up = WFCType.sand, right = WFCType.grass, down = WFCType.sand, left = WFCType.sand },
-            new WFCTile { type = WFCType.grass, up = WFCType.sand, right = WFCType.sand, down = WFCType.grass, left = WFCType.sand },
-            new WFCTile { type = WFCType.grass, up = WFCType.sand, right = WFCType.sand, down = WFCType.sand, left = WFCType.grass },
+            new WFCTile { type = WFCType.grass, up = WFCType.water, right = WFCType.grass, down = WFCType.sand, left = WFCType.grass},
 
             // Sand: 
-            //new WFCTile { type = WFCType.sand, up = WFCType.sand, right = WFCType.sand, down = WFCType.sand, left = WFCType.sand },
-            // Sand: 1 Grass
-            new WFCTile { type = WFCType.sand, up = WFCType.grass, right = WFCType.sand, down = WFCType.sand, left = WFCType.sand },
-            new WFCTile { type = WFCType.sand, up = WFCType.sand, right = WFCType.grass, down = WFCType.sand, left = WFCType.sand },
-            new WFCTile { type = WFCType.sand, up = WFCType.sand, right = WFCType.sand, down = WFCType.grass, left = WFCType.sand },
-            new WFCTile { type = WFCType.sand, up = WFCType.sand, right = WFCType.sand, down = WFCType.sand, left = WFCType.grass },
-            // Sand: 2 Grass
-            new WFCTile { type = WFCType.sand, up = WFCType.grass, right = WFCType.grass, down = WFCType.sand, left = WFCType.sand },
-            new WFCTile { type = WFCType.sand, up = WFCType.grass, right = WFCType.sand, down = WFCType.grass, left = WFCType.sand },
-            new WFCTile { type = WFCType.sand, up = WFCType.grass, right = WFCType.sand, down = WFCType.sand, left = WFCType.grass },
-            new WFCTile { type = WFCType.sand, up = WFCType.grass, right = WFCType.grass, down = WFCType.sand, left = WFCType.sand },
-            new WFCTile { type = WFCType.sand, up = WFCType.sand, right = WFCType.grass, down = WFCType.grass, left = WFCType.sand },
-            new WFCTile { type = WFCType.sand, up = WFCType.sand, right = WFCType.grass, down = WFCType.sand, left = WFCType.grass },
-            new WFCTile { type = WFCType.sand, up = WFCType.grass, right = WFCType.sand, down = WFCType.grass, left = WFCType.sand },
-            new WFCTile { type = WFCType.sand, up = WFCType.sand, right = WFCType.grass, down = WFCType.grass, left = WFCType.sand },
-            new WFCTile { type = WFCType.sand, up = WFCType.sand, right = WFCType.sand, down = WFCType.grass, left = WFCType.grass },
-            new WFCTile { type = WFCType.sand, up = WFCType.grass, right = WFCType.sand, down = WFCType.sand, left = WFCType.grass },
-            new WFCTile { type = WFCType.sand, up = WFCType.sand, right = WFCType.grass, down = WFCType.sand, left = WFCType.grass },
-            new WFCTile { type = WFCType.sand, up = WFCType.sand, right = WFCType.sand, down = WFCType.grass, left = WFCType.grass },
-            // Sand: 3 Grass
-            new WFCTile { type = WFCType.sand, up = WFCType.grass, right = WFCType.sand, down = WFCType.sand, left = WFCType.sand },
-            new WFCTile { type = WFCType.sand, up = WFCType.sand, right = WFCType.grass, down = WFCType.sand, left = WFCType.sand },
-            new WFCTile { type = WFCType.sand, up = WFCType.sand, right = WFCType.sand, down = WFCType.grass, left = WFCType.sand },
-            new WFCTile { type = WFCType.sand, up = WFCType.sand, right = WFCType.sand, down = WFCType.sand, left = WFCType.grass },
+            new WFCTile { type = WFCType.sand, up = WFCType.grass, right = WFCType.sand, down = WFCType.water, left = WFCType.sand },
 
             // Water: 
-            new WFCTile { type = WFCType.water, up = WFCType.water, right = WFCType.water, down = WFCType.water, left = WFCType.water },
-            // Water: 1 Sand
-            new WFCTile { type = WFCType.water, up = WFCType.sand, right = WFCType.water, down = WFCType.water, left = WFCType.water },
-            new WFCTile { type = WFCType.water, up = WFCType.water, right = WFCType.sand, down = WFCType.water, left = WFCType.water },
-            new WFCTile { type = WFCType.water, up = WFCType.water, right = WFCType.water, down = WFCType.sand, left = WFCType.water },
-            new WFCTile { type = WFCType.water, up = WFCType.water, right = WFCType.water, down = WFCType.water, left = WFCType.sand },
-            // Water: 2 Sand
-            new WFCTile { type = WFCType.water, up = WFCType.water, right = WFCType.water, down = WFCType.sand, left = WFCType.sand },
-            new WFCTile { type = WFCType.water, up = WFCType.water, right = WFCType.sand, down = WFCType.water, left = WFCType.sand },
-            new WFCTile { type = WFCType.water, up = WFCType.water, right = WFCType.sand, down = WFCType.sand, left = WFCType.water },
-            new WFCTile { type = WFCType.water, up = WFCType.water, right = WFCType.water, down = WFCType.sand, left = WFCType.sand },
-            new WFCTile { type = WFCType.water, up = WFCType.sand, right = WFCType.water, down = WFCType.water, left = WFCType.sand },
-            new WFCTile { type = WFCType.water, up = WFCType.sand, right = WFCType.water, down = WFCType.sand, left = WFCType.water },
-            new WFCTile { type = WFCType.water, up = WFCType.water, right = WFCType.sand, down = WFCType.water, left = WFCType.sand },
-            new WFCTile { type = WFCType.water, up = WFCType.sand, right = WFCType.water, down = WFCType.water, left = WFCType.sand },
-            new WFCTile { type = WFCType.water, up = WFCType.sand, right = WFCType.sand, down = WFCType.water, left = WFCType.water },
-            new WFCTile { type = WFCType.water, up = WFCType.water, right = WFCType.sand, down = WFCType.sand, left = WFCType.water },
-            new WFCTile { type = WFCType.water, up = WFCType.sand, right = WFCType.water, down = WFCType.sand, left = WFCType.water },
-            new WFCTile { type = WFCType.water, up = WFCType.sand, right = WFCType.sand, down = WFCType.water, left = WFCType.water },
-            // Water: 3 Sand
-            new WFCTile { type = WFCType.water, up = WFCType.water, right = WFCType.sand, down = WFCType.sand, left = WFCType.sand },
-            new WFCTile { type = WFCType.water, up = WFCType.sand, right = WFCType.water, down = WFCType.sand, left = WFCType.sand },
-            new WFCTile { type = WFCType.water, up = WFCType.sand, right = WFCType.sand, down = WFCType.water, left = WFCType.sand },
-            new WFCTile { type = WFCType.water, up = WFCType.sand, right = WFCType.sand, down = WFCType.sand, left = WFCType.water },
+            new WFCTile { type = WFCType.water, up = WFCType.sand, right = WFCType.water, down = WFCType.grass, left = WFCType.water },
         };
     }
 }
